f7734f09e8d6702e1a070817ec33a5820ee42d4d
Felix Maldonado <felix_maldonado@ncci.com>
2021-09-15 10:43:14 -0400

    Added fetch from the server

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,6 +4,7 @@ import { Breadcrumb, BreadcrumbItem, Modal, ModalHeader, ModalBody, Label, Card,
 import { Link } from 'react-router-dom';
 import { Control, LocalForm, Errors } from 'react-redux-form';
 import {Loading} from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl'
 
 
 const required = val => val && val.length;
@@ -32,7 +33,7 @@ function RenderCampsite({campsite}){
     return(
         <div class = 'col-md-5 m-1'>
             <Card>
-                <CardImg top src={campsite.image} alt={campsite.name}/>
+                <CardImg top src={baseUrl + campsite.image} alt={campsite.name}/>
                 <CardBody>
                     <CardText>{campsite.description}</CardText>
                 </CardBody>
